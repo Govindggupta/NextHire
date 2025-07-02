120719194739126e91e110d87672ead8ad0f1e7f
Govindggupta <govind12771277@gmail.com>
2025-07-02 18:10:01 +0000

    next.js setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return <>
+  <div>
+    <h1 className="text-3xl font-bold flex justify-center items-center min-h-screen min-w-screen">Landing page</h1>
+    <p>
+      This is a landing page for the ai interview platform.
+    </p>
+  </div>
+  </>
+}
