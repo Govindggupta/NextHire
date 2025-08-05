475d486f6effe32d14cd15a866b8f7624e124528
Govindggupta <govind12771277@gmail.com>
2025-08-05 18:05:35 +0000

    feat: nextauth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+
+  const session = await getServerSession();
+  
   return <>
   <div>
     <h1 className="text-3xl font-bold flex justify-center items-center min-h-screen min-w-screen">Landing page</h1>
     <p>
       This is a landing page for the ai interview platform.
     </p>
+
+    {session && (
+      <p>
+        You are signed in as {session?.user?.email}
+      </p>
+    )}
+
   </div>
   </>
 }
